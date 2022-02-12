64911d6bf08d0c4787b2d15f00b64382dc2130fd
rosa2070 <rosa2070@naver.com>
2022-02-12 23:49:26 +0900

    ddd

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -14,6 +14,10 @@ function LandingPage() {
             , width: '100%', height: '100vh'
         }}>
             <h2>시작 페이지</h2>
+
+            <button>
+                로그아웃
+            </button>
         </div>
     )
 }
