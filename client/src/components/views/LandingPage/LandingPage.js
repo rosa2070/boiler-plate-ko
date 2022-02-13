13ba60483528b15260df8d2e71757281627dec6a
rosa2070 <rosa2070@naver.com>
2022-02-13 23:56:39 +0900

    ddddd

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
 
-function LandingPage() {
+function LandingPage(props) {
 
     useEffect(() => {
         axios.get('/api/hello')
@@ -11,7 +11,11 @@ function LandingPage() {
     const onClickHandler = () => {
         axios.get(`/api/users/logout`)
             .then(response => {
-                console.log(response.data)
+                if (response.data.success) {
+                    props.history.push("/login")
+                } else {
+                    alert("로그아웃 하는데 실패 했습니다.")
+                }
             })
     }
 
---
+++ client/src/hoc/auth.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+
+export default function (SpecificComponent, option, adminRoute = null) {
+
+    useEffect(() => {
+        Axios.get
+    }, [])
+
+    return AuthenticationCheck
+}
