a68c4bcb410731028b577163ff58b53ee66cc856
rosa2070 <rosa2070@naver.com>
2022-02-14 21:46:17 +0900

    dddd

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
           of them to render at a time
         */}
         <Switch>
-          <Route exact path="/" component={Auth(LandingPage, null, true)} />
+          <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
         </Switch>
